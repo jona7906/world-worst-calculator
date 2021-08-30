aab9301365df994e30721503cda0d47861dfa011
JonasRossen <jona7906@stud.kea.dk>
2021-08-30 03:22:30 +0200

    final commit

--- script.js
+++ script.js
@@ -82,8 +82,10 @@ function showCalc(){
 }
 
 document.querySelector("#clear").addEventListener("click", clearOutput);
+
 function clearOutput(){
 console.log("clear output fields");
 document.querySelector("#results").textContent = "";
+document.querySelector("#results").textContent = calculation;
 }
 
